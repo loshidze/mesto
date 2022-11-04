09a04ef1a062f0099167820be666657ec4633f76
Sergey Belevkin <box5551@yandex.ru>
2022-11-04 05:08:03 +0300

    Проектная работа 4.2

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,42 @@
-let profileElement = document.querySelector('.profile__button-info');
-let popupElement = document.querySelector('.popup');
-let popupCloseElement = popupElement.querySelector('.popup__close-btn');
+const profileElement = document.querySelector('.profile__button-info');
+const popupElement = document.querySelector('.popup');
+const popupCloseElement = popupElement.querySelector('.popup__close-btn');
 
-let formElement = popupElement.querySelector('.popup__form');
-let nameInput = formElement.querySelector('#contentname');
-let jobInput = formElement.querySelector('#occupation');
+const formElement = popupElement.querySelector('.popup__form');
+const nameInput = formElement.querySelector('#contentname');
+const jobInput = formElement.querySelector('#occupation');
 
-let profileTitle = document.querySelector('.profile__title');
-let profileSubtitle = document.querySelector('.profile__subtitle');
+const profileTitle = document.querySelector('.profile__title');
+const profileSubtitle = document.querySelector('.profile__subtitle');
 
-profileElement.addEventListener('click', function() {
+// profileElement.addEventListener('click', function() {
+//   popupElement.classList.add('popup_opened');
+//   nameInput.value = profileTitle.textContent;
+//   jobInput.value = profileSubtitle.textContent;
+// });
+
+// popupCloseElement.addEventListener('click', function () {
+//   popupElement.classList.remove('popup_opened');
+// });
+
+// function formSubmitHandler (evt) {
+//   evt.preventDefault();
+//   profileTitle.textContent = nameInput.value;
+//   profileSubtitle.textContent = jobInput.value;
+//   popupElement.classList.remove('popup_opened');
+// }
+
+// formElement.addEventListener('submit', formSubmitHandler);
+
+function openPopup() {
   popupElement.classList.add('popup_opened');
   nameInput.value = profileTitle.textContent;
   jobInput.value = profileSubtitle.textContent;
-});
+}
 
-popupCloseElement.addEventListener('click', function () {
+function closePopup() {
   popupElement.classList.remove('popup_opened');
-});
+}
 
 function formSubmitHandler (evt) {
   evt.preventDefault();
@@ -26,5 +45,6 @@ function formSubmitHandler (evt) {
   popupElement.classList.remove('popup_opened');
 }
 
+profileElement.addEventListener('click', openPopup);
+popupCloseElement.addEventListener('click', closePopup);
 formElement.addEventListener('submit', formSubmitHandler);
-
